a554aa8d7b41caba77c44dd6fefc25739acf06ee
mo0nchild <happytown77831@gmail.com>
2023-10-24 01:06:20 +0300

    fix: main branch clear

--- java-progtech-task0/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module application.javaprogtechtask0 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens application.domen to javafx.fxml;
-    exports application.domen;
-    exports application.models;
-}
--- java-progtech-task2/src/main/java/application/models/shape/commons/IShapeFactoryFromName.java
+++
@@ -1,7 +0,0 @@
-package application.models.shape.commons;
-
-import application.models.shape.Shape;
-
-public interface IShapeFactoryFromName {
-    public Shape createShape(String name) throws ShapeFactoryException;
-}
