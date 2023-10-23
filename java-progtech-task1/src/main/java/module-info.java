80bce6e6f6ebcf9d37509b0a6fed02db084cc3d7
mo0nchild <happytown77831@gmail.com>
2023-10-24 01:25:30 +0300

    fix: develop branch clear

--- java-progtech-task1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module application.javaprogtechtask1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.jetbrains.annotations;
-
-    opens application.domen to javafx.fxml;
-    exports application.domen;
-    exports application.models;
-}
--- java-progtech-task2/src/main/java/application/services/repository/commons/IRepositoryFactory.java
+++
@@ -1,7 +0,0 @@
-package application.services.repository.commons;
-
-import application.services.repository.IRepository;
-
-public interface IRepositoryFactory<TItem> {
-    public IRepository<TItem> getFactory();
-}
