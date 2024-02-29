a46aeaab86212318c619b5f9885add09dfeb9823
mo0nchild <happytown77831@gmail.com>
2024-02-29 22:24:07 +0300

    init: create project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module application.domen.task14chainresp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens application.domen.task14chainresp to javafx.fxml;
+    exports application.domen.task14chainresp;
+}
