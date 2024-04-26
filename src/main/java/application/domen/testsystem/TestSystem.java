f36b0def868ddb188ffaf3656b29236ee2e8ff8c
mo0nchild <happytown77831@gmail.com>
2024-04-26 17:03:50 +0300

    feat: change log text color

--- src/main/java/application/domen/testsystem/TestSystem.java
+++ src/main/java/application/domen/testsystem/TestSystem.java
@@ -12,6 +12,9 @@ import java.util.List;
 
 @NoArgsConstructor
 public class TestSystem implements ITestSystem {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     private List<Method> getTestMethods(Class<? extends TestClass> testClass) {
         var testMethods = new ArrayList<Method>();
         for (var item : testClass.getMethods()) {
@@ -40,15 +43,15 @@ public class TestSystem implements ITestSystem {
             var methodName = this.getTestMethodName(method);
             try {
                 method.invoke(testObject);
-                System.out.printf("+ [%s]: { Method test complete }\n", methodName);
+                System.out.printf("%s+ [%s]: { Method test complete }\u001B[37m\n", ANSI_GREEN, methodName);
             }
             catch (InvocationTargetException error) {
                 if(!(error.getCause() instanceof TestAssertException)) throw new RuntimeException(error);
-                System.out.printf("- [%s]: { %s }\n", methodName, error.getCause().getMessage());
+                System.out.printf("%s- [%s]: { %s }\u001B[37m\n", ANSI_RED, methodName, error.getCause().getMessage());
             }
             catch (ReflectiveOperationException error) { throw new RuntimeException(error); }
             this.getLogsFromMethod(testObject).forEach(message -> {
-                System.out.printf("\t[LogInfo]: ( %s )\n", message);
+                System.out.printf("\t[LogInfo]: ( %s )\n\n", message);
             });
         };
     }
